8b67fd8780b769fb7b736731ca97f94cc2f43c1e
kaushikkamarasu <kamarasukaushik9@gmail.com>
2024-10-02 22:34:54 +0530

    Made some changes

--- src/components/DisplayCards.jsx
+++ src/components/DisplayCards.jsx
@@ -1,43 +1,45 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'; // Changed useHistory to useNavigate
 import {
-    Container,
-    Card,
-    CardHeader,
-    Heading,
-    Button,
-    Image,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalCloseButton,
-    ModalBody,
-    ModalFooter,
-    useDisclosure,
-    CardFooter,
-    HStack,
-    Spacer
+    Card, CardHeader, CardFooter, HStack, Heading, Spacer, Image, Button,
+    Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody,
+    ModalFooter, FormControl, FormLabel, Input, AlertDialog,
+    AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogBody,
+    AlertDialogFooter, Container, useDisclosure
 } from '@chakra-ui/react';
 
-
-//fetch assets
+// Fetch assets
 import projectImage from '../assets/project.png';
 
 export const DisplayCards = () => {
+    const navigate = useNavigate(); // Updated for React Router v6
 
-    //overlay setup
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const [modalMessage, setModalMessage] = useState('');
+    // Modal and Alert Dialog hooks
+    const { isOpen: isModalOpen, onOpen: onModalOpen, onClose: onModalClose } = useDisclosure();
+    const { isOpen: isAlertOpen, onOpen: onAlertOpen, onClose: onAlertClose } = useDisclosure();
 
-    //handling ineligibilities
-    const handleViewDetails = (projectType) => {
-        if (projectType === 'major') {
-            setModalMessage('You are have already been alloted a Guide!');
-        } else {
-            setModalMessage('You are not eligible to apply , because you have gotten an Internship!');
+    // Internship Form State
+    const [internshipDetails, setInternshipDetails] = useState({
+        duration: '',
+        companyName: '',
+        managerName: '',
+        studentName: ''
+    });
+
+    // Handling form input changes
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setInternshipDetails(prevState => ({ ...prevState, [name]: value }));
+    };
+
+    // Handling Internship decision
+    const handleInternshipDecision = (decision) => {
+        if (decision === 'yes') {
+            onAlertClose(); // Close alert dialog
+            onModalOpen();  // Open modal to fill in internship details
+        } else if (decision === 'no') {
+            navigate('/minor-allocation'); // Redirect to preference page
         }
-        onOpen();
     };
 
     return (
@@ -46,13 +48,14 @@ export const DisplayCards = () => {
                 maxW={{ base: "95%", sm: "90%", md: "80%", lg: "container.lg", xl: "container.xl" }}
                 my="120px"
                 mx="auto"
-                // Center the container horizontally
                 alignContent="center"
                 justifyContent="center"
             >
                 <Heading fontFamily="'Sanchez', serif" textAlign="center">
                     Project Details Are Given Below
                 </Heading>
+
+
 
                 {/* Major Project Card */}
                 <Card
@@ -74,9 +77,11 @@ export const DisplayCards = () => {
                         </HStack>
                     </CardHeader>
                     <CardFooter px="10" mb="2" fontFamily="'Poppins', serif">
-                        <Button mr="5px"
+                        {/* First Apply Button triggers modal */}
+                        <Button
+                            mr="5px"
                             colorScheme="orange"
-                            onClick={() => handleViewDetails('major')}
+                            onClick={onOpen}  // Opens the modal
                             _hover={{ bg: 'orange.300', color: 'white' }}
                             mb={{ base: 2, md: 0 }}
                         >
@@ -92,6 +97,7 @@ export const DisplayCards = () => {
                         </Link>
                     </CardFooter>
                 </Card>
+
 
                 {/* Minor Project Card */}
                 <Card
@@ -113,19 +119,17 @@ export const DisplayCards = () => {
                         </HStack>
                     </CardHeader>
                     <CardFooter px="10" mb="2" fontFamily="'Poppins', serif">
-                        <Button mr="5px"
+                        <Button
+                            mr="5px"
                             colorScheme="orange"
-                            onClick={() => handleViewDetails('minor')}
+                            onClick={onAlertOpen} // Open internship decision dialog
                             _hover={{ bg: 'orange.300', color: 'white' }}
                             mb={{ base: 2, md: 0 }}
                         >
                             Apply
                         </Button>
                         <Link to="/minor-allocation">
-                            <Button ml="5px"
-                                colorScheme="orange"
-                                _hover={{ bg: 'orange.300', color: 'white' }}
-                            >
+                            <Button ml="5px" colorScheme="orange" _hover={{ bg: 'orange.300', color: 'white' }}>
                                 Apply
                             </Button>
                         </Link>
@@ -133,27 +137,108 @@ export const DisplayCards = () => {
                 </Card>
             </Container>
 
+            {/* Alert Dialog for Internship Question */}
+            <AlertDialog
+                isOpen={isAlertOpen}
+                leastDestructiveRef={undefined}
+                onClose={onAlertClose}
+            >
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                            Internship Confirmation
+                        </AlertDialogHeader>
 
+                        <AlertDialogBody>
+                            Have you done an internship?
+                        </AlertDialogBody>
 
-            {/* Modal for View Details */}
+                        <AlertDialogFooter>
+                            <Button onClick={() => handleInternshipDecision('no')} colorScheme="orange">
+                                No
+                            </Button>
+                            <Button onClick={() => handleInternshipDecision('yes')} ml={3}>
+                                Yes
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
+
+            {/* Modal for Internship Details */}
+            <Modal isOpen={isModalOpen} onClose={onModalClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Internship Details</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <FormControl>
+                            <FormLabel>Duration</FormLabel>
+                            <Input
+                                placeholder="Enter duration"
+                                name="duration"
+                                value={internshipDetails.duration}
+                                onChange={handleInputChange}
+                            />
+                        </FormControl>
+
+                        <FormControl mt={4}>
+                            <FormLabel>Company Name</FormLabel>
+                            <Input
+                                placeholder="Enter company name"
+                                name="companyName"
+                                value={internshipDetails.companyName}
+                                onChange={handleInputChange}
+                            />
+                        </FormControl>
+
+                        <FormControl mt={4}>
+                            <FormLabel>Manager Name</FormLabel>
+                            <Input
+                                placeholder="Enter manager name"
+                                name="managerName"
+                                value={internshipDetails.managerName}
+                                onChange={handleInputChange}
+                            />
+                        </FormControl>
+
+                        <FormControl mt={4}>
+                            <FormLabel>Student Name</FormLabel>
+                            <Input
+                                placeholder="Enter your name"
+                                name="studentName"
+                                value={internshipDetails.studentName}
+                                onChange={handleInputChange}
+                            />
+                        </FormControl>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme="orange" mr={3} onClick={onModalClose}>
+                            Submit
+                        </Button>
+                        <Button onClick={onModalClose}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+            {/* Modal for "You have been allotted a Guide!" */}
             <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
-                <ModalContent bg="white">
-                    <ModalHeader>Your Details</ModalHeader>
+                <ModalContent>
+                    <ModalHeader>Notice</ModalHeader>
                     <ModalCloseButton />
-                    <ModalBody color="black" fontFamily="'Poppins', serif">
-                        {modalMessage}
+                    <ModalBody>
+                        You have been allotted a Guide!
                     </ModalBody>
                     <ModalFooter>
                         <Button colorScheme="orange" onClick={onClose}>
-                            Close
+                            Okay
                         </Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
         </>
     );
-}
+};
 
-
-
+export default DisplayCards;
